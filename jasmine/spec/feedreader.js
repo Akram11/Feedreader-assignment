5bcd432a3bee9ddcf92d05ca3b9df16283685465
Akram <akram.alfawakheeri@bms.bmsoft.de>
2019-01-04 19:07:22 +0100

    Test: added a "menu" test suit it test that the menu is hidden by default

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,17 +9,12 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+    /* This suite is all about the RSS
+     * feeds definitions, the allFeeds variable in our application
+     */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
+        /* This tests that the allFeeds variable has been defined
+         * and that it is not empty.
          */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
@@ -49,14 +44,23 @@ $(function() {
          });
     });
 
+    /* This suite is all about the Menu
+     */
 
+    describe('The menu', function() {
+
+      /* This test ensures the menu element is
+       * hidden by default.
+       */
+      it('menu is hidden',function() {
+        let body =  document.querySelector('body');
+        expect(body.classList.contains('menu-hidden')).toBe(true);
+      });
+
+    });
     /* TODO: Write a new test suite named "The menu" */
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
